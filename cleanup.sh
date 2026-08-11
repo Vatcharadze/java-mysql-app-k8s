49600da75a824ba4457aef3947a7343895903863
dato <dato.vatcharadze@gmail.com>
2026-08-11 22:28:30 +0400

    fixed file location

--- cleanup.sh
+++ cleanup.sh
@@ -9,27 +9,27 @@ NC='\033[0m'
 echo "Starting Kubernetes cleanup..."
 
 echo "Deleting Ingress..."
-kubectl delete -f app/java-mysql-app-ingress.yaml --ignore-not-found=true
+kubectl delete -f java-k8s/java-mysql-app-ingress.yaml --ignore-not-found=true
 
 echo "Deleting PHP Deployment..."
-kubectl delete -f mysql/php-deployment.yaml --ignore-not-found=true
+kubectl delete -f mysql-k8s/php-deployment.yaml --ignore-not-found=true
 
 echo "Deleting Java Deployment..."
-kubectl delete -f app/java-deployment.yaml --ignore-not-found=true
+kubectl delete -f java-k8s/java-deployment.yaml --ignore-not-found=true
 
 echo "Deleting Java App Secret..."
-kubectl delete -f app/java-secret.yaml --ignore-not-found=true
+kubectl delete -f java-k8s/java-secret.yaml --ignore-not-found=true
 
 echo "Deleting MySQL Deployment..."
-kubectl delete -f mysql/mysql-deployment.yaml --ignore-not-found=true
+kubectl delete -f mysql-k8s/mysql-deployment.yaml --ignore-not-found=true
 
 echo "Deleting ConfigMap..."
-kubectl delete -f mysql/db-configmap.yaml --ignore-not-found=true
+kubectl delete -f mysql-k8s/db-configmap.yaml --ignore-not-found=true
 
 echo "Deleting MySQL Persistent Volume Claim..."
-kubectl delete -f mysql/mysql-pvc.yaml --ignore-not-found=true
+kubectl delete -f mysql-k8s/mysql-pvc.yaml --ignore-not-found=true
 
 echo "Deleting MySQL Secret..."
-kubectl delete -f mysql/mysql-secret.yaml --ignore-not-found=true
+kubectl delete -f mysql-k8s/mysql-secret.yaml --ignore-not-found=true
 
 echo -e "${GREEN}Cleanup Complete!${NC}"
--- run-script.sh
+++ run-script.sh
@@ -16,27 +16,27 @@ fi
 echo "Applying Kubernetes files..."
 
 echo "Creating MySQL Secret..."
-kubectl apply -f mysql/mysql-secret.yaml
+kubectl apply -f mysql-k8s/mysql-secret.yaml
 
 echo "Creating MySQL Persistent Volume..."
-kubectl apply -f mysql/mysql-pvc.yaml
+kubectl apply -f mysql-k8s/mysql-pvc.yaml
 
 echo "Creating ConfigMap..."
-kubectl apply -f mysql/db-configmap.yaml
+kubectl apply -f mysql-k8s/db-configmap.yaml
 
 echo "Creating MySQL Deployment..."
-kubectl apply -f mysql/mysql-deployment.yaml
+kubectl apply -f mysql-k8s/mysql-deployment.yaml
 
 echo "Creating Java App Secret..."
-kubectl apply -f app/java-secret.yaml
+kubectl apply -f java-k8s/java-secret.yaml
 
 echo "Creating Java Deployment..."
-kubectl apply -f app/java-deployment.yaml
+kubectl apply -f java-k8s/java-deployment.yaml
 
 echo "Creating PHP Deployment..."
-kubectl apply -f mysql/php-deployment.yaml
+kubectl apply -f mysql-k8s/php-deployment.yaml
 
 echo "Creating Ingress..."
-kubectl apply -f app/java-mysql-app-ingress.yaml
+kubectl apply -f java-k8s/java-mysql-app-ingress.yaml
 
 echo -e "${GREEN}Deployment completed successfully!${NC}"
